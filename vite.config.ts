1daa24389f86089b98dc66e298c0d780c4f416a2
yuhldr <yuhldr@qq.com>
2025-12-20 23:04:57 +0800

    perf: 支持压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { compression, defineAlgorithm } from "vite-plugin-compression2";
+import { constants } from "node:zlib";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    compression({
+      threshold: 1024, // >1KB 才压（建议）
+      algorithms: [
+        defineAlgorithm("gzip", { level: 9 }),
+        defineAlgorithm("brotliCompress", {
+          params: {
+            [constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        }),
+      ],
+      // deleteOriginalAssets: false, // 默认就是 false，不建议删源文件
+    }),
+  ],
   base: "/ldr_atoms_viewer/",
 });
